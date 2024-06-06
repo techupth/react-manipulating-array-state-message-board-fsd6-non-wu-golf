b60789772fe3a4299f74cefc269bbcfa6d93bf53
Apirak Fakin <apirakfakin@gmail.com>
2024-06-06 13:39:58 +0700

    feat: add states for inputMessage and messages board

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,52 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [inputMessage, setInputMessage] = useState("");
+  const [messages, setMessages] = useState(["Hello", "Hi"]);
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            onChange={(e) => setInputMessage(e.target.value)}
+            value={inputMessage}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button
+          className="submit-message-button"
+          onClick={() => {
+            if (inputMessage.trim() !== "") {
+              setMessages([...messages, inputMessage]);
+              setInputMessage("");
+            }
+          }}
+        >
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {messages.map((item, index) => {
+          return (
+            <div className="message" key={index}>
+              <h1>{item}</h1>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  let delMessage = [...messages];
+                  delMessage.splice(index, 1);
+                  setMessages(delMessage);
+                }}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
